8e751efd6c6910f71d06ffb914d132e8d5ea8af4
rodulph.huang <rudolph.huang@tron.network>
2022-06-16 17:31:29 +0800

    init

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # ERC20 subgraph
 
-
 https://thegraph.com/studio/subgraph/apenft-token/
 
-```
-query{
-  users(first:10, orderBy:balance,orderDirection:desc){
-    id
-    balance
-  }
+查询 0x13ebc0a1fee2f7e5a1809712957229b70fd77bf2 代币 持有量前5名
+
+```graphql
+{
+    balances(first:5,where:{tokenAddress:"0x13ebc0a1fee2f7e5a1809712957229b70fd77bf2"}, orderBy:amount,orderDirection :desc){
+        tokenAddress
+        user{
+            id
+        }
+        amount
+    }
 }
 ```
--- src/mappings/token.ts
+++ src/mappings/token.ts
@@ -1,8 +1,12 @@
 import {
+  ERC20,
   Transfer as TransferEvent,
   Approval as ApprovalEvent,
 } from '../../generated/TestToken/ERC20';
-import { getToken, getTransfer, getAllowance, getUser } from './helpers';
+
+import {Address, BigInt, Bytes} from '@graphprotocol/graph-ts';
+
+import {getToken, getTransfer, getAllowance, getUser, getBalance} from './helpers';
 
 export function handleTransfer(event: TransferEvent): void {
   let txHash = event.transaction.hash;
@@ -25,6 +29,22 @@ export function handleTransfer(event: TransferEvent): void {
   transfer.value = event.params.value;
   transfer.token = token.id;
   transfer.save();
+
+  let erc20 = ERC20.bind(address);
+  let balance_from = erc20.try_balanceOf(event.params.from);
+  let balance_to = erc20.try_balanceOf(event.params.to);
+
+  let balance_from_entity = getBalance(event.params.from,address)
+  balance_from_entity.user = fromUser.id;
+  balance_from_entity.tokenAddress = address;
+  balance_from_entity.amount =  balance_from.reverted ? BigInt.fromI32(0) : balance_from.value;
+  balance_from_entity.save()
+
+  let balance_to_entity = getBalance(event.params.to,address)
+  balance_to_entity.user = toUser.id;
+  balance_to_entity.tokenAddress = address;
+  balance_to_entity.amount =  balance_from.reverted ? BigInt.fromI32(0) : balance_to.value;
+  balance_to_entity.save()
 }
 
 export function handleApproval(event: ApprovalEvent): void {
